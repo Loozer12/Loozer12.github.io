f229bf914c1a3790c599cd793e1e589a3b6dfff7
█٩×̯×۶█ █٩×̯×۶█ <loozer12@yandex.ru>
2021-01-26 15:22:37 +0400

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>
+        Калькулятор Инженерный
+    </title>
+    
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
+    
+    <script type="text/javascript" src="script.js"></script>
+    
+</head>
+<body>
+    <p id="header">
+        <span>КАЛЬКУЛЯТОР ИНЖЕНЕРНЫЙ</span>
+    </p>
+</body>
+</html>
