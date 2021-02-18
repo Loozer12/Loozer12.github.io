c18c3cf2d84d1b7864cb3420e212dec5f4a40f22
█٩×̯×۶█ █٩×̯×۶█ <loozer12@yandex.ru>
2021-02-18 18:15:23 +0400

    dev

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 ﻿class CalcLogic {
 	numberNow = 1;
 	numberSignNow = "+";
-	number1 = "";
-	number2 = "";
+	number1 = "0";
+	number2 = "0";
 	methodNow = null;
 	result = null;
+	isEnd = false;
 	notation = "DEC";
+	maxLen = 12;
 	
 	constructor()
 	{
@@ -27,19 +29,79 @@
 	{
 		let viewNum = NaN;
 		
+		this.updateMemAction();
+
 		if(this.result == null)
 		{
-			viewNum = this["number" + this.numberNow];
+			if(this["number" + this.numberNow] == "" || this["number" + this.numberNow] == "-"){
+				this["number" + this.numberNow] = "0";
+				this.numberSignNow = "+";
+				viewNum = 0;
+			}else{
+				viewNum = this["number" + this.numberNow];
+			}
 		}
-		else
+		else if(this.result!= NaN && this.result != Infinity)
 		{
-			viewNum = this.result;
+			if(this.result==0){
+				viewNum = "0";
+			}else if(Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+				viewNum = this.result.toExponential();
+			}else{
+				viewNum = this.noExp(this.result);
+				if(viewNum.indexOf(".")>=0){
+					viewNum = this.roundNum(viewNum);
+				}
+			}
 		}
-		
+
 		if(this.notation == "HEX")
 			viewNum = "neDopilil";
-			
+
+		this["number" + this.numberNow] = "" + viewNum;
 		$("#window").val(viewNum);
+	}
+
+	updateMemAction(mode){
+		if(this.methodNow == null && mode!="noClear"){
+			$("#memAction")[0].innerText = "";
+		}else{
+			$("#memAction")[0].innerText = this.roundNum((+this.number1).toString()) + " " + this.methodNow;
+		}
+	}
+
+	checkEnd(){
+		if(this.isEnd){
+			this.btnPressClean();
+			this.isEnd = false;
+		}
+	}
+
+	checkResult(){
+		if(this.result != null){
+			/*
+			if(this.numberNow==1){
+				this.number1 = "" + this.result;
+			}
+			else{
+				this.number1 = this.number2;
+			}
+			this.numberNow = 1;
+			this.numberSignNow = this.number1[0]=='-'?"-":"+";*/
+			this.result = null;
+		}
+	}
+
+	checkAction(){
+		if(this.methodNow!=null && this.numberNow==2){
+			this.btnPressResult("clear");
+		}
+	}
+
+	checkRules(mode){
+		this.checkEnd();
+		this.checkResult();
+		if(mode=="binary")this.checkAction();
 	}
 	
 	_btnAnimation(name)
@@ -51,15 +113,21 @@
 	}
 
 	btnPressNum(num) {
-		
-		if(this["number" + this.numberNow].length < 12)
+
+		this.checkRules();
+
+		if(this["number" + this.numberNow].length - (this["number"+this.numberNow].indexOf(".")==-1?0:1) < this.maxLen)
 		{
 			if(!isNaN(Number(num)))
 			{
 				let number = Number(num);
 				this._btnAnimation(number);
 				
-				this["number" + this.numberNow] = this["number" + this.numberNow] + "" + number;
+				if(this["number" + this.numberNow]==0 && this["number" + this.numberNow].toString().indexOf(".")==-1){
+					this["number" + this.numberNow] = "" +  number;
+				}else{
+					this["number" + this.numberNow] = this["number" + this.numberNow] + "" + number;
+				}
 				
 				this.updateInput();
 			}
@@ -70,32 +138,43 @@
 		}
 		else
 		{
-			console.error(`Func name: btnPressNum\n\nError: var number${this.numberNow} - length more than 12`);
+			console.info(`Func name: btnPressNum\n\nInfo: var number${this.numberNow} - length more than ${this.maxLen}`);
 		}		
 	}
 	
 	btnPressBackspace()
 	{
+
+		this.checkRules();
+
 		this._btnAnimation("Backspace");
 		
-		if(this.result == null)
-		{
-			let str = this["number" + this.numberNow] + "";
+		//if(this.result == null)
+		//{
+		let str = this["number" + this.numberNow] + "";
+	
+		this["number" + this.numberNow] = str.substr(0, str.length - 1);
 		
-			this["number" + this.numberNow] = str.substr(0, str.length - 1);
-			
-			this.updateInput();
-		}
+		this.updateInput();
+		/*}
 		else
 		{
-			this.btnPressClear();
-		}
+			this.btnPressClean();
+		}*/
 	}
 	
 	btnPressCleanEntry()
 	{
+
+		this.checkRules();
+
 		this._btnAnimation("CleanEntry");
-		
+
+		this["number" + this.numberNow] = "0";
+
+		this.updateInput();
+
+		/*
 		if(this.result == null)
 		{
 			this["number" + this.numberNow] = "";
@@ -112,15 +191,16 @@
 			this.result = null;
 			
 			this.updateInput();
-		}
+		}*/
+
 	}
 	
 	btnPressClean()
 	{
 		this.numberNow = 1;
 		this.numberSignNow = "+";
-		this.number1 = "";
-		this.number2 = "";
+		this.number1 = "0";
+		this.number2 = "0";
 		this.methodNow = null;
 		this.result = null;
 		
@@ -129,6 +209,9 @@
 	
 	btnPressDot()
 	{
+
+		this.checkRules();
+
 		this._btnAnimation("Dot");
 		
 		let str = this["number" + this.numberNow] + "";
@@ -147,10 +230,18 @@
 	
 	btnPressSwapSign()
 	{
+
+		this.checkRules();
+
 		if(this["number" + this.numberNow].length == 0)
 			return console.log("Нет числа");
 		
 		this._btnAnimation("Sign");
+
+		if(this["number" + this.numberNow] == "0"){
+			this.numberSignNow == "+";
+			return;
+		}
 		
 		if(this.numberSignNow == "+")
 		{
@@ -172,11 +263,13 @@
 	
 	btnPressPlus()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("Plus");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" + this.result;
 			this.number2 = "";
 			this.result = null;
 		}	
@@ -192,11 +285,14 @@
 	
 	btnPressMinus()
 	{
+
+		this.checkRules("binary");
+
 		this._btnAnimation("Minus");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" +  this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -212,11 +308,13 @@
 	
 	btnPressStar()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("Star");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" +  this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -232,11 +330,13 @@
 	
 	btnPressSlash()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("Slash");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" +  this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -252,11 +352,13 @@
 	
 	btnPressDiv()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("Div");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" +  this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -272,11 +374,13 @@
 	
 	btnPressMod()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("Mod");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 = "" + this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -292,16 +396,18 @@
 	
 	btnPressSqrtXY()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("SqrtXY");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 ="" +  this.result;
 			this.number2 = "";
 			this.result = null;
 		}
 		
-		this.methodNow = "newSqrt";
+		this.methodNow = "sqrt";
 		
 		this.numberNow = 2;
 		
@@ -312,11 +418,13 @@
 	
 	btnPressPowerXY()
 	{
+		this.checkRules("binary");
+
 		this._btnAnimation("PowerXY");
 		
 		if(this.result != null)
 		{
-			this.number1 = this.result;
+			this.number1 = "" + this.result;
 			this.number2 = "";
 			this.result = null;
 		}
@@ -332,26 +440,36 @@
 	
 	btnPressSqrtX2()
 	{
+		this.checkRules();
+
 		this._btnAnimation("SqrtX2");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
 			this.result = null;
 		}
 		
-		this.methodNow = "newSqrt";
+		this.methodNow = "sqrt";
 		
 		this.numberNow = 2;
 		this.number2 = "2";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.pow(this["number" + this.numberNow],1/2);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressPowerX2()
 	{
+		this.checkRules();
+
 		this._btnAnimation("PowerX2");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -363,13 +481,22 @@
 		this.numberNow = 2;
 		this.number2 = "2";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.pow(this["number" + this.numberNow],2);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
-	btnPressPowerX1()
+	btnPressPowerXm1()
 	{
+		this.checkRules();
+
 		this._btnAnimation("PowerX-1");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -381,31 +508,50 @@
 		this.numberNow = 2;
 		this.number2 = "-1";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.pow(this["number" + this.numberNow],-1);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressSqrtX3()
 	{
+		this.checkRules();
+
 		this._btnAnimation("SqrtX3");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
 			this.result = null;
 		}
 		
-		this.methodNow = "newSqrt";
+		this.methodNow = "sqrt";
 		
 		this.numberNow = 2;
 		this.number2 = "3";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.pow(this["number" + this.numberNow],1/3);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressSin()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Sin");
 		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -417,13 +563,21 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.sin(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressCos()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Cos");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -435,13 +589,21 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.cos(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressTg()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Tg");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -453,13 +615,21 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.tan(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressCtg()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Ctg");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -471,13 +641,21 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.ctan(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressArcsin()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Arcsin");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -489,13 +667,21 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+		this.result = Math.asin(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressArccos()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Arccos");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -507,13 +693,22 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.acos(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressArctg()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Arctg");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -525,13 +720,22 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.atan(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
 	btnPressArcctg()
 	{
+		this.checkRules();
+
 		this._btnAnimation("Arcctg");
-		
+		/*
 		if(this.result != null)
 		{
 			this.number1 = this.result;
@@ -543,10 +747,17 @@
 		this.numberNow = 2;
 		this.number2 = "0";
 		
-		this.btnPressResult();
+		this.btnPressResult();*/
+
+		this.result = Math.actan(this["number" + this.numberNow]*Math.PI/180);
+		this["number" + this.numberNow] = ""+this.result;
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+		this.updateInput();
 	}
 	
-	btnPressResult()
+	btnPressResult(mode)
 	{
 		if(this.number1.length == 0 || this.number2.length == 0 || this.methodNow == null)
 			return console.log("Нет нужных элементов для решения");
@@ -560,25 +771,19 @@
 			case "*":
 			case "/":
 				tmpRes = eval(`${this.number1} ${this.methodNow} ${this.number2}`);
-				
+				/*
 				if(!isNaN(tmpRes))
 					this.result = tmpRes.toFixed(2);
-				else
-					this.result = tmpRes;
-				
-				this.updateInput();
+				else*/
+				this.result = tmpRes;
 			break;
 			
 			case "mod":
 				this.result = this.number1 % this.number2;
-				
-				this.updateInput();
 			break;
 			
 			case "div":
 				this.result = parseInt(this.number1 / this.number2);
-				
-				this.updateInput();
 			break;
 			
 			case "sin":
@@ -589,42 +794,88 @@
 			case "acos":
 			case "atan":
 			case "actan":
-				tmpRes = Math[this.methodNow](this.number1);
-				
+				tmpRes = Math[this.methodNow](this["number" + this.numberNow]*Math.PI/180);
+				/*
 				if(!isNaN(tmpRes))
 					this.result = tmpRes.toFixed(2);
-				else
-					this.result = tmpRes;
-				
-				this.updateInput();
+				else*/
+				this.result = tmpRes;
 			break;
 			
-			case "newSqrt":
-				tmpRes = this.sqrt(this.number1,this.number2);
+			case "sqrt":
+				tmpRes = Math.pow(this.number1,1/this.number2);
 				
-				if(!isNaN(tmpRes))
+				/*if(!isNaN(tmpRes))
 					this.result = tmpRes.toFixed(2);
-				else
-					this.result = tmpRes;
-				
-				this.updateInput();
+				else*/
+				this.result = tmpRes;
 			break;
 			
 			case "power":
 				tmpRes = Math.pow(this.number1,this.number2);
-				
+				/*
 				if(!isNaN(tmpRes))
 					this.result = tmpRes.toFixed(2);
-				else
-					this.result = tmpRes;
-				
-				this.updateInput();
+				else*/
+				this.result = tmpRes;
 			break;
-			
+		}
+
+
+		if(isNaN(this.result) || this.result == Infinity || Math.abs(this.result)>999999999999 || Math.abs(this.result)<0.0000000001){
+			this.isEnd = true;
+		}
+
+		this.numberNow = 1;
+		this.number2 = "0";
+		this.methodNow = null;
+
+		if(mode=="clear" && !this.isEnd){
+			//this.updateMemAction("noClear");
+		}else{
+			this.updateInput();
 		}
 	}
-	
-	
+
+	UnaryResult(){
+			
+	}
+
+	noExp(num){
+		if(Math.abs(num)<1){
+			if(num.toString().indexOf("e")>=0){
+				if(num>=0){
+					return "0"+(num+1).toString().substr(1);
+				}else{
+					return "-0"+(num-1).toString().substr(2);
+				}
+			}
+		}
+		return ""+num;
+	}
+
+	roundNum(numStr){
+		let n1=0;
+		let n2=0;
+		let f=false
+
+		for(let i=0;i<numStr.length;i++){
+			if(numStr[i]=="-")continue;
+			if(numStr[i]=="."){
+				f=true;
+			}else if(!f){
+				n1++;
+			}else{
+				n2++;
+			}
+		}
+
+		if(f==false || n1+n2<=this.maxLen){
+			return "" + numStr;
+		}else{
+			return this.noExp((+numStr).toFixed(this.maxLen-n1)).replace(/0*$/,"");
+		}
+	}
 }
 
 let Calc = new CalcLogic();
@@ -789,7 +1040,7 @@ $("#btnPowerX2").click((e)=>{
 });
 
 $("#btnPowerX-1").click((e)=>{
-	Calc.btnPressPowerX1();
+	Calc.btnPressPowerXm1();
 });
 
 
